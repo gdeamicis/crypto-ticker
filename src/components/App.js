1355bed9e5635781e272c110f12e886f86cb20d5
Gonzalo De Amicis <gdeamicis11@gmail.com>
2018-07-15 12:28:31 +0200

    set up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../App.css';
-import PropTypes from 'prop-types'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import MarketPage from '../containers/marketContainer';
+import LiquidityPage from '../containers/liquidityContainer';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <MarketPage />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Link to={`/`}>
+              Market Overview
+          </Link>
+            <Link to={`/liquidity`}>
+              Liquidity
+          </Link>
+          </header>
+          <div>
+            <Route path="/" component={MarketPage} />
+            <Route path="/liquidity" component={LiquidityPage} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
